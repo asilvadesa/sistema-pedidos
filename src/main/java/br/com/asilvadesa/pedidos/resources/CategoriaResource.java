36ec051748dc45a10e7e8ceffd92676a8351778d
Anderson Silva <asilvadesa@gmail.com>
2020-09-10 23:11:10 -0400

    Testando a primeira classe de dominio

--- src/main/java/br/com/asilvadesa/pedidos/resources/CategoriaResource.java
+++ src/main/java/br/com/asilvadesa/pedidos/resources/CategoriaResource.java
@@ -1,16 +1,25 @@
 package br.com.asilvadesa.pedidos.resources;
 
+import br.com.asilvadesa.pedidos.domain.Categoria;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
     @RequestMapping(method = RequestMethod.GET)
-    public String listar(){
-        return "Rest OK";
+    public List<Categoria> listar(){
+        Categoria cat1 = new Categoria(1,"Informática");
+        Categoria cat2 = new Categoria(1,"Escritório");
+
+        List<Categoria> lista = new ArrayList<>(Arrays.asList(cat1, cat2));
+        return  lista;
     }
 
 }
